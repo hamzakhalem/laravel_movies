8a3bc8546a094234f8d51243b5fb30b00b56d9c0
hamza <hamzakhalem97@gmail.com>
2024-01-10 19:47:11 +0100

    show page modals

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -94,19 +94,24 @@
         <h2 class="text-4xl font-semibold">Cast</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             @foreach ($movie['credits']['cast'] as $cast)
-            @if ($loop->index < 5)
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="{{'https://image.tmdb.org/t/p/w300/'. $cast['profile_path'] }}" alt="actor1" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $cast['name'] }}</a>
-                        <div class="text-sm text-gray-400">
-                            {{ $cast['character'] }}
+                @if ($loop->index < 5)
+                    <div class="mt-8">
+                        <a href="#">
+                            <img src="{{'https://image.tmdb.org/t/p/w300/'. $cast['profile_path'] }}" alt="actor1" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="mt-2">
+                            <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $cast['name'] }}</a>
+                            <div class="text-sm text-gray-400">
+                                {{ $cast['character'] }}
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endif
+ 
+                @else
+                    @break
+
+                @endif
+                
             @endforeach
         </div>
     </div>
@@ -129,6 +134,8 @@
                         <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                 </div>
+            @else
+                @break
             @endif
             @endforeach
         </div>
