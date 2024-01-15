189ab95dd56e3953365dda0a74af06eef6022926
hamza <hamzakhalem97@gmail.com>
2024-01-15 18:51:05 +0100

    new fixing

--- app/Http/Controllers/ActorsController.php
+++ app/Http/Controllers/ActorsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\ViewModels\ActorsViewModel;
+use App\ViewModels\ActorViewModel;
 
 class ActorsController extends Controller
 {
@@ -18,4 +19,25 @@ class ActorsController extends Controller
         $viewModel = new ActorsViewModel($popularActors, $page);
         return view("actors.index", $viewModel);
     }
+
+    public function show($id)
+    {
+        $actor = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id)
+            ->json();
+
+        $social = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/external_ids')
+            ->json();
+
+        $credits = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/combined_credits')
+            ->json();
+
+        $viewModel = new ActorViewModel($actor, $social, $credits);
+
+        return view('actors.show', $viewModel);
+    }
+
 }
+
